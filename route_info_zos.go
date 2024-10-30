a7498e0c2d90bcbf8be667a944319cb7e5a50d7c
Joon Lee <joon.s.lee@ibm.com>
2024-10-30 12:58:59 -0400

    Add support for z/OS

---
+++ route_info_zos.go
@@ -0,0 +1,42 @@
+// +build zos
+package sockaddr
+
+import (
+   "errors"
+   "os/exec"
+   "regexp"
+   "strings"
+)
+
+var defaultRouteRE *regexp.Regexp = regexp.MustCompile(`^Default +([0-9\.\:]+) +([^ ]+) +([0-9]+) +([^ ]+)`)
+
+
+// zosGetDefaultInterfaceName executes the onetstat command and returns its output
+func zosGetDefaultInterfaceName() (string, error) {
+	out, err := exec.Command("/bin/onetstat", "-r").Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+// zosProcessOneStatOutput processes the output of onetstat -r and returns the default interface name
+func zosProcessOneStatOutput(output string) (string, error) {
+	linesout := strings.Split(output, "\n")
+	for _, line := range linesout {
+		result := defaultRouteRE.FindStringSubmatch(line)
+		if result != nil {
+			return result[4], nil
+		}
+	}
+	return "", errors.New("no default interface found")
+}
+
+// GetDefaultInterfaceName returns the interface name attached to the default route
+func (ri routeInfo) GetDefaultInterfaceName() (string, error) {
+	output, err := zosGetDefaultInterfaceName()
+	if err != nil {
+		return "", err
+	}
+	return zosProcessOneStatOutput(output)
+}
